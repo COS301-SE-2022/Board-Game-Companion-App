218fe8d54a2a4e6e517a8929b09f847c40c4c2c8
Joseph-Makgopa <u18166793@tuks.co.za>
2022-05-06 17:38:20 +0200

    added new project files

---
+++ apps/client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'board-game-companion-app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'client';
+}
