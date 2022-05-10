ef58c2a77716243aca8c899268bb2e6622060005
MR Marsden <u18080368@tuks.co.za>
2022-05-10 23:12:32 +0200

    🎨(UI): Adding header to client side display
    Adding the header to the side display.

--- apps/client/src/app/app.module.ts
+++ apps/client/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 import { PagesModule } from '@board-game-companion-app/pages';
+import { SharedModule } from '@board-game-companion-app/shared';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
-    PagesModule
+    PagesModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent],
