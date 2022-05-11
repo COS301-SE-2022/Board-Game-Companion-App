4abf58b4c8603789f063c475eef4d19292a44175
NasiphiM <u18074074@tuks.co.za>
2022-05-11 21:33:26 +0200

    🏷️ Modified files

--- libs/pages/src/index.ts
+++ libs/pages/src/index.ts
@@ -1,3 +1,5 @@
 export * from './lib/pages.module';
 
 export * from './lib/home/home.component';
+
+export * from './lib/login/login.component';
--- libs/pages/src/lib/pages.module.ts
+++ libs/pages/src/lib/pages.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [HomeComponent],
-  exports: [HomeComponent],
+  declarations: [HomeComponent, LoginComponent],
+  exports: [HomeComponent, LoginComponent],
 })
 export class PagesModule {}
