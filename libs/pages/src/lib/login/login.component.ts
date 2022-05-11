4e4654aac4df138602316e552967366abaa5867a
NasiphiM <u18074074@tuks.co.za>
2022-05-11 21:21:08 +0200

    Login Page Component

---
+++ libs/pages/src/lib/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'board-game-companion-app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
