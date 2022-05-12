a09a801a6e5dca43804bcff98553105aa0c24b81
KyleHaarhoff <u19033347@tuks.co.za>
2022-05-12 15:11:17 +0200

    :construction:Added carousel as child And created app-routing

--- libs/pages/src/lib/home/home.component.ts
+++ libs/pages/src/lib/home/home.component.ts
@@ -4,9 +4,14 @@ import { Component, OnInit } from '@angular/core';
   selector: 'board-game-companion-app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  
 })
 export class HomeComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    
+    
+
+  }
 }
