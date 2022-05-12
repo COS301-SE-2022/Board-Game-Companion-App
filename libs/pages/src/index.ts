9aa9f41304bddadb89650f4e224d8484500dc201
NasiphiM <u18074074@tuks.co.za>
2022-05-12 06:56:32 +0200

    🚧 Login Page Componets

--- libs/pages/src/index.ts
+++ libs/pages/src/index.ts
@@ -3,3 +3,5 @@ export * from './lib/pages.module';
 export * from './lib/home/home.component';
 
 export * from './lib/board-game-details/board-game-details.component';
+
+export * from './lib/login/login.component';
--- libs/pages/src/lib/pages.module.ts
+++ libs/pages/src/lib/pages.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { BoardGameDetailsComponent } from './board-game-details/board-game-details.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [HomeComponent, BoardGameDetailsComponent],
-  exports: [HomeComponent, BoardGameDetailsComponent],
+  declarations: [HomeComponent, BoardGameDetailsComponent, LoginComponent],
+  exports: [HomeComponent, BoardGameDetailsComponent, LoginComponent],
 })
 export class PagesModule {}
