19c1359e0c3929e96767bfa5d744f5ea49cae3c6
NasiphiM <u18074074@tuks.co.za>
2022-05-12 21:24:49 +0200

    💄 Login Page Components

--- libs/pages/src/lib/login/login.component.ts
+++ libs/pages/src/lib/login/login.component.ts
@@ -1,12 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { SocialAuthService } from 'angularx-social-login';
+//import { ConnectableObservable } from 'rxjs';
+import { GoogleLoginProvider } from "angularx-social-login";
 
 @Component({
   selector: 'board-game-companion-app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
-  constructor() {}
+export class LoginComponent {
+  //user:any;
+  constructor(private authService:SocialAuthService) {}
+  
 
-  ngOnInit(): void {}
+  //ngOnInit(): void {}
+  //sign to sign in with google
+
+  signUserIn():void
+  {
+    /*platform = GoogleLoginProvider.PROVIDER_ID;
+    this._socialAuthServ.signIn(platform).then((response)=>{
+      console.log(platform + "logged in user data is=" ,response);
+
+      this.user =response;*/
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+    
+
+  signAdminIn():void
+  {
+    /*platform = GoogleLoginProvider.PROVIDER_ID;
+    this._socialAuthServ.signIn(platform).then((response)=>{
+      console.log(platform + "logged in user data is=" ,response);
+
+      this.user =response;
+    }
+    );*/
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+
 }
+
--- libs/pages/src/lib/pages.module.ts
+++ libs/pages/src/lib/pages.module.ts
@@ -3,10 +3,51 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { BoardGameDetailsComponent } from './board-game-details/board-game-details.component';
 import { LoginComponent } from './login/login.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { SocialLoginModule, SocialAuthServiceConfig } from 'angularx-social-login'; 
+import{ GoogleLoginProvider} from 'angularx-social-login';
+
+const google_oauth_client_id = "13912431210-vfi8j8pi30e1ree2j3f3vsfb4o762p3t.apps.googleusercontent.com";
+//user:any;
+
+const configure = [
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider(google_oauth_client_id)
+  }
+];
+
+export function provideConfig(){
+  return configure;
+}
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, BrowserModule, SocialLoginModule],
   declarations: [HomeComponent, BoardGameDetailsComponent, LoginComponent],
-  exports: [HomeComponent, BoardGameDetailsComponent, LoginComponent],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              'clientId'
+            )
+            /*provide: AuthServiceConfig,
+            useFactory: provideConfig*/
+          },
+         
+        ],
+        onError: (err: any) => {
+          console.error(err);
+        } 
+      }as SocialAuthServiceConfig,
+    }
+  ],
+  exports: [HomeComponent, BoardGameDetailsComponent, LoginComponent]
 })
+
 export class PagesModule {}
+
