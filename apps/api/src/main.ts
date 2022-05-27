b8818ea896727917af128d76677abfc22bfb61e3
Joseph-Makgopa <u18166793@tuks.co.za>
2022-05-27 11:55:04 +0200

    ✨ (swagger):added swagger package

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,7 +5,7 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
@@ -13,6 +13,16 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
+  
+  const config = new DocumentBuilder()
+    .setTitle('Board Game Companion API')
+    .setDescription('The api specification for the board game companion app')
+    .setVersion('1.0')
+    .addTag('BGC')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
