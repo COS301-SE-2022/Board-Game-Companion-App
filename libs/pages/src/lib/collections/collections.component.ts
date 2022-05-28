913e6db989863bea7a4a282e60a94a0d4605c71e
KyleHaarhoff <u19033347@tuks.co.za>
2022-05-29 01:24:11 +0200

    Added collections display

--- libs/pages/src/lib/collections/collections.component.ts
+++ libs/pages/src/lib/collections/collections.component.ts
@@ -1,12 +1,70 @@
 import { Component, OnInit } from '@angular/core';
 
+// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
+import { BggSearchService } from 'libs/shared/src/lib/services/bgg-search.service';
+
+
 @Component({
   selector: 'board-game-companion-app-collections',
   templateUrl: './collections.component.html',
   styleUrls: ['./collections.component.scss'],
 })
 export class CollectionsComponent implements OnInit {
-  constructor() {}
+  constructor(private bggSearch:BggSearchService) {}
 
-  ngOnInit(): void {}
+  collections: collectionList[] = new Array<collectionList>();
+
+  ngOnInit(): void {
+
+    //get collections
+
+    //get images for things in collection
+
+    if(localStorage.getItem("collections") === null ||localStorage.getItem("collections") == "[]")
+    {
+      //there are no collections
+    }
+    else
+    {
+      let  names:string[] = JSON.parse(localStorage.getItem("collections")||"");
+
+      for(let i=0; i<names.length;i++)
+      {
+        //get the 
+        let ids:string[] = JSON.parse(localStorage.getItem(names[i])||"");
+        let urls:string[] = new Array<string>();
+        for(let j=0; j<ids.length;j++)
+        {
+          if(ids[j]!=null)
+          {
+            this.bggSearch.getComments("https://boardgamegeek.com/xmlapi2/thing?id="+ids[j])
+                .subscribe(
+                  data=>{
+                  let result:string = data.toString();
+                  let parseXml = new window.DOMParser().parseFromString(result, "text/xml");
+                  parseXml.querySelectorAll("image").forEach(imgUrl=>{
+                    urls.push(imgUrl.innerHTML);
+                });
+
+
+          });
+          }
+        }
+
+        this.collections.push(new collectionList(names[i],urls));
+      }
+    }
+    console.log(this.collections)
+  }
+}
+class collectionList
+{
+    public name:String = "";
+    public imgUrls:String[] = [];
+
+    constructor( n:string, i:string[])
+    {
+      this.name=n;
+      this.imgUrls = i;
+    }
 }
