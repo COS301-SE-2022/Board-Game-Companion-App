35dec95cde1e534cb7f57125bce4b0e008222dd3
KyleHaarhoff <u19033347@tuks.co.za>
2022-06-08 00:46:32 +0200

    :poop: Login working

--- libs/client-lib/src/lib/shared/components/header/header.component.ts
+++ libs/client-lib/src/lib/shared/components/header/header.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
+import { GoogleAuthService, userDetails} from 'libs/api-lib/src/lib/services/GoogleAuth/google-auth.service';
+
+
 
 @Component({
   selector: 'board-game-companion-app-header',
@@ -7,14 +11,55 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  constructor(private readonly router:Router) {}
+  UserDetails: userDetails | undefined;
+  log = "login";
 
-  ngOnInit(): void {}
+  constructor(private readonly router:Router, private readonly gapi: GoogleAuthService) {
+    
+
+    gapi.UserSubject.subscribe(details=>{
+      this.UserDetails = details;
+      console.log(this.UserDetails);
+    })
+  }
+
+  ngOnInit(): void {
+
+    if(this.gapi.isLoggedIn())
+    {
+      const el = document.getElementById('l');
+      if(el!=null)
+      {
+        el.innerHTML = "logout";
+
+        this.log = "logout";
+      }
+      
+        
+        
+      
+      
+
+    }
+    
+  }
 
   moveTo(path:string):void{
     if(path == "collection")
     {
       this.router.navigate(['/home']);
+    }
+    else if(path=="login")
+    {
+      if(this.log == "login")
+      {
+        this.router.navigate(['/' + path]);
+      }
+      else
+      {
+        this.gapi.signOut();
+        this.router.navigate(['/home']);
+      }
     }
     else{
       this.router.navigate(['/' + path]);
--- libs/client-lib/src/lib/shared/shared.module.ts
+++ libs/client-lib/src/lib/shared/shared.module.ts
@@ -3,7 +3,9 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BggSearchService } from './services/bgg-search/bgg-search.service';
-
+// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
+import { GoogleAuthService } from 'libs/api-lib/src/lib/services/GoogleAuth/google-auth.service';
+import { OAuthModule } from 'angular-oauth2-oidc';
 @NgModule({
   declarations: [
     FooterComponent,
@@ -11,10 +13,14 @@ import { BggSearchService } from './services/bgg-search/bgg-search.service';
     HeaderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    
+    OAuthModule.forRoot(),
+    
   ],
   providers: [
-    BggSearchService
+    BggSearchService,
+    GoogleAuthService
   ],
   exports: [
     HeaderComponent,
