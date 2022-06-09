fcd7160eb69a84a78b78de27bba28488ca146da7
NasiphiM <u18074074@tuks.co.za>
2022-06-09 23:55:07 +0200

    💄 (editor): Proper Edit Page

--- libs/client-lib/src/lib/editor/editor/editor.component.ts
+++ libs/client-lib/src/lib/editor/editor/editor.component.ts
@@ -4,6 +4,7 @@ import { AsyncSubject, Subject } from 'rxjs';
 import tinymce from 'tinymce/tinymce';
 import { EditorService } from '../editor-service/editor.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Body } from '@nestjs/common';
 
 
 @Component({
@@ -11,22 +12,25 @@ import { ActivatedRoute, Router } from '@angular/router';
   templateUrl: './editor.component.html',
   styleUrls: ['./editor.component.scss'],
 })
-export class EditorComponent {
+export class EditorComponent implements OnInit{
   title = 'angular-richtexteditor'
   id = ""
+  texteditor : any;
+  lineCountCache = 0;
+  lineNum : any;
   constructor(private edtservice: EditorService, private router:Router, private route: ActivatedRoute){
     //this.router.getCurrentNavigation()?.currentState.extras.state?.author
-
-		const data = this.router.getCurrentNavigation()?.extras.state;
-    console.log(data)
+    const data = this.router.getCurrentNavigation()?.extras.state;
 
     if (data?.['id'] && data?.['filename']){
       this.id = data?.['id']
       this.getFile(data?.['id'], data?.['filename'])
     }
+    console.log(data)
 
     //this.router.getCurrentNavigation()?.extras.state?.filename
   }
+  
   private formsbjct: Subject<any> = new AsyncSubject();
 
   public editForm = new FormGroup({
@@ -35,13 +39,55 @@ export class EditorComponent {
     FileName: new FormControl("", Validators.required)
   })
   
+  ngOnInit(): void {
+    this.texteditor = document.getElementById('editorTextarea');
+    this.lineNum = document.getElementById('lineNumber');
+		
+
+
+    this.texteditor?.addEventListener('scroll', () => {
+      if (this.lineNum){
+        this.lineNum.scrollTop = this.texteditor.scrollTop;
+        this.lineNum.scrollLeft = this.texteditor.scrollLeft;
+      } 
+    });
+    
+    throw new Error('Method not implemented.');
+  }
+  addTab(event:any): void{
+    console.log(this.texteditor);
+    event.preventDefault();
+    let val =  this.texteditor?.value;
+    const selectionStart =  this.texteditor?.selectionStart ;
+    const selectionEnd =  this.texteditor?.selectionEnd ;
+    //let val = this.editForm.get("body")?.value;
+    val = val?.slice(0, selectionStart) + '\t' + val?.slice(selectionEnd);
+    this.texteditor?.setSelectionRange(selectionStart+2, selectionStart+2)
+    this.editForm.get("body")?.setValue(val)
+  }
+
+  addLine(event:any): void{
+    const lineCount = this.texteditor.value.split('\n').length;
+      const outarr = [];
+      if (this.lineCountCache != lineCount) {
+         for (let x = 0; x < lineCount; x++) {
+            outarr[x] = (x + 1) + '.';
+         }
+         this.lineNum.value = outarr.join('\n');
+      }
+      this.lineCountCache = lineCount;
+  }
+  
   onUpload(): void{
-    console.log('this download button works');
+    console.log('this Upload button works');
     const content = this.editForm.get("body")?.value;
+    console.log(content);
+
+
     const fname = this.editForm.get("FileName")?.value;
     this.edtservice.postScript(this.id, fname, content).subscribe(data=>{console.log(data)});
     
-    console.log(content);
+    
 
     //setting the body
     //this.editForm.get("body")?.patchValue("The dog is out of the house");
@@ -51,6 +97,10 @@ export class EditorComponent {
 
   onCreate(): void{
     console.log("This is the button to push script content to script");
+   
+    const content = this.editForm.get("body")?.value;
+
+    console.log(content);
   }
 
   onExecute():void{
