46c53d52958b502a1d11070c3719a16b1d3e4ad0
MR Marsden <u18080368@tuks.co.za>
2022-06-10 06:43:18 +0200

    💄(UI): Game sessions Update, style update
    update to sessions and styles for other pages

--- libs/client-lib/src/lib/script-executor/script-executor/script-executor.component.ts
+++ libs/client-lib/src/lib/script-executor/script-executor/script-executor.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ScriptService } from '../../shared/services/scripts/script.service';
+import { Router } from '@angular/router';
+import { timer } from 'rxjs';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'board-game-companion-app-script-executor',
@@ -8,12 +11,58 @@ import { ScriptService } from '../../shared/services/scripts/script.service';
   styleUrls: ['./script-executor.component.scss'],
 })
 export class ScriptExecutorComponent implements OnInit {
-  constructor(private route: ActivatedRoute,private readonly scriptService:ScriptService) {}
-
+  constructor(private route: ActivatedRoute,private readonly scriptService:ScriptService, private router:Router) {}
   scriptID = "";
   file = "test";
   folder = "42a58303-990e-4230-94c6-a9f5dd629500"
+  hours = "hours";
+  h:number = 0;
+  min = "min";
+  m:number = 0;
+  
+  
+
+  
+  
+  
+  back()
+  {
+   //Timer
+    let c = JSON.parse(localStorage.getItem("sessions")||"")
+    let name = "#" + (c.length + 1);
+    c.push(name);
+    localStorage.setItem("sessions", JSON.stringify(c))
+    let session = []
+    session.push("tictactoe10")
+    this.scriptID = this.route.snapshot.paramMap.get('my_object')||"";
+    session.push(this.scriptID)
+    session.push("2")
+    session.push("N/A")
+    this.hours = this.h + this.hours + " "
+    this.min = this.m + this.min
+    session.push(this.hours + this.min)
+    session.push("Win")
+    const now = new Date();
+    session.push(now.toLocaleDateString())
+    localStorage.setItem(name, JSON.stringify(session))
+    
+    this.router.navigate(['scripts']);
+  }
+
+  
   ngOnInit(): void {
+
+    
+
+ 
+  setInterval(() => {
+    this.m++
+    if(this.m === 60)
+    {
+      this.h++
+      this.m = 0
+    }
+  }, 1000);
 
     //get id
     this.scriptID = this.route.snapshot.paramMap.get('my_object')||"";
