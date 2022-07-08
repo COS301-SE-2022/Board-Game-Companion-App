e1fb0734aec9f3a4a5785280b5cf69b80929e1a5
QabaNjabulo <u19062665@tuks.co.za>
2022-07-08 07:07:27 +0200

    🚧 Added pagination to the table.

--- libs/client-lib/src/lib/admin/admin/admin.component.spec.ts
+++ libs/client-lib/src/lib/admin/admin/admin.component.spec.ts
@@ -1,3 +1,4 @@
+import { NgxPaginationModule } from 'ngx-pagination';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
@@ -11,7 +12,7 @@ describe('AdminComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AdminComponent],
-      imports: [HttpClientTestingModule,RouterTestingModule]
+      imports: [HttpClientTestingModule,RouterTestingModule,NgxPaginationModule]
     }).compileComponents();
   });
 
