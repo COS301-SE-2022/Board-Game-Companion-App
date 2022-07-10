4e32488f80acf2145cb1a6cdd5da2cc6387cc826
KyleHaarhoff <u19033347@tuks.co.za>
2022-07-10 14:10:07 +0200

    :bug: Header fixed
    Header now automatically shows login/logout

--- libs/client-lib/src/lib/shared/components/header/header.component.ts
+++ libs/client-lib/src/lib/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, KeyValueDiffer, KeyValueDiffers, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
 import { GoogleAuthService, userDetails} from '../../../google-login/GoogleAuth/google-auth.service';
@@ -16,22 +16,26 @@ export class HeaderComponent implements OnInit {
   log = "login";
   loggedIn = false;
   admin:string[] = ["u18166793@tuks.co.za","u18080368@tuks.co.za","mattrmarsden@gmail.com"];
-
-  constructor(private readonly router:Router, private readonly gapi: GoogleAuthService) {
+  
+  differ: KeyValueDiffer<string, any>;
+  constructor(private readonly router:Router, private readonly gapi: GoogleAuthService, private differs: KeyValueDiffers) {
     
 
     gapi.UserSubject.subscribe(details=>{
       this.UserDetails = details;
-      console.log(this.UserDetails);
+      
     })
+    this.differ = this.differs.find({}).create();
   }
 
   ngOnInit(): void {
-
+    
     if(this.gapi.isLoggedIn())
     {
+      
       this.loggedIn = true;
       const el = document.getElementById('l');
+      console.log(el!=null)
       if(el!=null)
       {
         el.innerHTML = "logout";
@@ -74,6 +78,37 @@ export class HeaderComponent implements OnInit {
     }
     else{
       this.router.navigate(['/' + path]);
+    }
+  }
+
+  // eslint-disable-next-line @angular-eslint/use-lifecycle-interface
+  ngDoCheck() {
+    const change = this.differ.diff(this);
+    if (change) {
+      const el = document.getElementById('l');
+      if(this.gapi.isLoggedIn())
+    {
+      
+      this.loggedIn = true;
+      
+      
+      if(el!=null)
+      {
+        el.innerHTML = "logout";
+
+        this.log = "logout";
+      } 
+
+    }else
+    {
+      this.loggedIn = false;
+      if(el!=null)
+      {
+        el.innerHTML = "login";
+
+        this.log = "login";
+      } 
+    }
     }
   }
 }
