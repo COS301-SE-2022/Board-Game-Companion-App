27b564b1d519ab1a62ce9997d3e618ada6a82a1e
QabaNjabulo <u19062665@tuks.co.za>
2022-07-16 12:02:48 +0200

    tests for fetch and search models.

---
+++ libs/client-lib/src/lib/shared/models/model tests/fetch-session-results.spec.ts
@@ -0,0 +1,24 @@
+import { TestBed } from '@angular/core/testing';
+import { fetchSessionResults } from '../fetch-session-results';
+describe('search result model test',()=>{
+    let model: fetchSessionResults;
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+          declarations: [fetchSessionResults],
+        }).compileComponents();
+
+      });
+
+      it('should create ',()=>{
+        model = new fetchSessionResults("game","file","7","28-09-22","1","23-11","lose","12345");
+        expect(model).toBeDefined();
+        expect(model.getBoardGame()).toBe("game");
+        expect(model.getScript()).toBe("file");
+        expect(model.getTimePlayed()).toBe("7");
+        expect(model.getDate()).toBe("28-09-22");
+        expect(model.getScore()).toBe("23-11");
+        expect(model.getID()).toBe("12345");
+        expect(model.getResult()).toBe("lose");
+        expect(model.getNumOpponents()).toBe("1");
+      });
+});
