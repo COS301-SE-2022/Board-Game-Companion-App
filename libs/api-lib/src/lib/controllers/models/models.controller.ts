aa6f1ca93ce724ad3caffa2f5eddfd53150c9d7c
Joseph-Makgopa <u18166793@tuks.co.za>
2022-07-22 12:20:41 +0200

    (models):created model services in backend

--- libs/api-lib/src/lib/controllers/models/models.controller.ts
+++ libs/api-lib/src/lib/controllers/models/models.controller.ts
@@ -1,7 +1,8 @@
 import { Controller, Body,  Get, Query, Post, Put, Delete, Req ,UploadedFile, UseInterceptors, Param, HttpException, HttpStatus } from '@nestjs/common';
+import { ModelsService } from '../../services/models/models.service';
 
 @Controller('comments')
 export class ApiModelsController {
-    constructor(){}
-        
+    constructor(private readonly modelsService:ModelsService){}
+
 }
---
+++ libs/api-lib/src/lib/services/models/models.service.spec.ts
@@ -0,0 +1,33 @@
+import { getModelToken } from '@nestjs/mongoose';
+import { Test, TestingModule } from '@nestjs/testing';
+import { Comment } from '../../schemas/comment.schema';
+import { Like } from '../../schemas/like.schema';
+import { ModelsService } from './models.service';
+
+describe('CommentService', () => {
+  let service: ModelsService;
+
+  const mockRepository = {
+    find() {
+      return {};
+    }
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [ModelsService,   { 
+          provide: getModelToken(Comment.name),
+          useValue: mockRepository
+      },  { 
+        provide: getModelToken(Like.name),
+        useValue: mockRepository
+    }],
+    }).compile();
+
+    service = module.get<ModelsService>(ModelsService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
