5c6c00d7aad8a8d0607ac64ab9b8f41548a1ab3b
QabaNjabulo <u19062665@tuks.co.za>
2022-07-27 18:34:56 +0200

    ✅ Test all the routes.

--- libs/client-lib/src/lib/client-lib-routing.module.ts
+++ libs/client-lib/src/lib/client-lib-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
+export const routes: Routes = [
   {
     path: 'board-game-details',
     loadChildren: () => import('./board-game-details/board-game-details.module').then(m=>m.BoardGameDetailsModule),
--- libs/client-lib/src/lib/google-login/google-login.module.ts
+++ libs/client-lib/src/lib/google-login/google-login.module.ts
@@ -4,9 +4,7 @@ import { RouterModule } from '@angular/router';
 import { GoogleLoginComponent } from './google-login.component';
 import { HttpClientModule } from '@angular/common/http';
 import { OAuthModule } from 'angular-oauth2-oidc';
-
-// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
-import { GoogleAuthService } from 'libs/client-lib/src/lib/google-login/GoogleAuth/google-auth.service';
+import { GoogleAuthService } from './GoogleAuth/google-auth.service';
 
 @NgModule({
   declarations: [
