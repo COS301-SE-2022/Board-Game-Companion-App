205394980b4b46c599321412c5b0a201007db6fe
QabaNjabulo <u19062665@tuks.co.za>
2022-07-29 05:18:11 +0200

    🐛 change in author datatype.

--- libs/client-lib/src/lib/admin/admin/admin.component.ts
+++ libs/client-lib/src/lib/admin/admin/admin.component.ts
@@ -3,7 +3,7 @@ import { AdminService } from '../admin-service/admin.service';
 import { ActivatedRoute, Router } from '@angular/router';
 // import { StringMap } from '@angular/compiler/src/compiler_facade_interface';
 // import { script } from '../../shared/models/script';
-
+import { user } from '../../shared/models/user';
 // import { TestPassService } from '../../test-pass.service';
 // import { Router } from '@angular/router';
 
@@ -150,7 +150,7 @@ export class AdminComponent implements OnInit {
   onSearch(): void{
     this.scripts = [];
     this.adminService.getScripts().subscribe(data=>{
-      this.scripts = data.filter( (res: {name: string;}) => res.name.toLowerCase().includes(this.searchedValue.toLowerCase()));
+      this.scripts = data.filter( (res: {author: user;}) => res.author.name.toLowerCase().includes(this.searchedValue.toLowerCase()));
 
       for(let i=0; i < this.scripts.length; i++){
         const date = this.scripts[i].created.split(" ");
@@ -164,10 +164,10 @@ export class AdminComponent implements OnInit {
   onSort(): void{
     if(this.selected==="alphabetical" && this.scripts.length!==0){
 
-      this.scripts.sort(function(resultA: { name: string; }, resultB: { name: string; })
+      this.scripts.sort(function(resultA: { author: user; }, resultB: { author: user; })
       {
-        const nameA = resultA.name.toUpperCase(); // ignore upper and lowercase
-        const nameB = resultB.name.toUpperCase(); // ignore upper and lowercase
+        const nameA = resultA.author.name.toUpperCase(); // ignore upper and lowercase
+        const nameB = resultB.author.name.toUpperCase(); // ignore upper and lowercase
 
         if (nameA < nameB) 
         {
