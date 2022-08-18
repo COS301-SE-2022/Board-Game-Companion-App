987379f0e76616d7646705a68328a29a3f02344f
Joseph-Makgopa <u18166793@tuks.co.za>
2022-08-18 16:58:53 +0200

    (models):added functionality to input labels in pretrained models section

--- libs/client-lib/src/lib/models/upload/upload.component.ts
+++ libs/client-lib/src/lib/models/upload/upload.component.ts
@@ -18,11 +18,61 @@ import { StorageService } from '../../shared/services/storage/storage.service';
 })
 export class UploadComponent implements OnInit{
   @ViewChild(NotificationComponent,{static:true}) notifications: NotificationComponent = new NotificationComponent();
-  
+  name = "";  
+  location = 0;
+  type = "classification"
+  label = "";
+  labels:string[] = []
 
   constructor(private readonly modelsService:ModelsService,private readonly storageService:StorageService){}
 
   ngOnInit(): void{
-    console.log("upload")
+    this.name = "";
+  }
+
+  changeLocation(value:number): void{
+    this.location = value;
+  }
+
+  checkInputOnEnter(value:any): void{
+
+    if(value.key === "Enter"){
+      value?.preventDefault();
+      this.addLabel();
+    }
+  }
+
+  addLabel(): void{
+    if(this.label !== ""){
+      if(!this.alreadyExists(this.label)){
+        this.labels.push(this.label);
+      }else
+        this.notifications.add({type:'warning',message:'Duplicate label not allowed.'});
+
+      this.label = "";
+    }else
+      this.notifications.add({type:'warning',message:'Can not use empty label.'});
+  }
+
+  alreadyExists(value:string): boolean{
+    let result = false;
+
+    for(let count = 0; count < this.labels.length && !result; count++){
+      if(this.labels[count] === value)
+        result = true;
+    }
+
+    return result;
+  }
+
+  removeInput(value:string): void{
+    const temp:string[] = [];
+
+    for(let count = 0; count < this.labels.length; count++){
+      if(value!== this.labels[count])
+        temp.push(this.labels[count]);
+    }
+
+    this.labels = temp; 
   }
 }
