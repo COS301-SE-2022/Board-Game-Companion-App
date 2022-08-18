234fe428f742c5d81ce371ef8f2ad769cd882a49
Joseph-Makgopa <u18166793@tuks.co.za>
2022-08-18 17:47:26 +0200

    (model):implemented upload function in pretrained models section

--- libs/client-lib/src/lib/models/upload/upload.component.ts
+++ libs/client-lib/src/lib/models/upload/upload.component.ts
@@ -1,14 +1,10 @@
 import { Component, EventEmitter, OnInit,Output, ViewChild } from '@angular/core';
 import { ModelsService } from '../../shared/services/models/models.service';
-import { layer } from '../../shared/models/layer';
 import * as tf from '@tensorflow/tfjs'
-import { beginTraining } from '../../shared/models/beginTraining';
-import { LoadDataComponent } from '../load-data/load-data.component';
-import { ArchitectureComponent } from '../architecture/architecture.component';
-import { ConfigureComponent } from '../configure/configure.component';
 import { NotificationComponent } from '../../shared/components/notification/notification.component';
 import { modelData } from '../../shared/models/modelData';
 import { StorageService } from '../../shared/services/storage/storage.service';
+import { user } from '../../shared/models/user';
 
 @Component({
   selector: 'board-game-companion-app-upload',
@@ -22,6 +18,7 @@ export class UploadComponent implements OnInit{
   location = 0;
   type = "classification"
   label = "";
+  path = "";
   labels:string[] = []
 
   constructor(private readonly modelsService:ModelsService,private readonly storageService:StorageService){}
@@ -74,5 +71,89 @@ export class UploadComponent implements OnInit{
     }
 
     this.labels = temp; 
+  }
+
+  upload(): void{
+    if(this.name === ""){
+      this.notifications.add({type: "danger",message: "The name of the neural network is missing."});
+      return;
+    }
+
+    if(this.type === "classification" && this.labels.length === 0){
+      this.notifications.add({type: "danger",message: "Classification model does not have any labels."});
+    }
+
+    switch(this.location){
+      case 0:{
+        tf.loadLayersModel(this.path).then((value:tf.LayersModel) => {
+          const user:user = {
+            name: sessionStorage.getItem("name") as string,
+            email: sessionStorage.getItem("email") as string
+          }
+
+          let temp = "";
+
+          if(this.labels.length !== 0){
+            temp = "&labels=" + JSON.stringify(this.labels);
+          }
+
+          value.save(`http://localhost:3333/api/models/create?userName=${user.name}&userEmail=${user.email}&name=${this.name}&created=${(new Date()).toString()}&type=${this.type}${temp}`).then((value:tf.io.SaveResult)=>{
+            this.notifications.add({type: "success",message: "Successfully uploaded model."});
+          }).catch((reason:any)=>{
+            this.notifications.add({type:"danger",message:"Failed to upload model"})
+          })
+
+        }).catch((reason:any) => {
+          this.notifications.add({type: "danger", message: "Failed to load model."})
+        })
+      }break;
+      case 1:{
+        tf.loadLayersModel(`indexeddb://${this.path}`).then((value:tf.LayersModel) => {
+          const user:user = {
+            name: sessionStorage.getItem("name") as string,
+            email: sessionStorage.getItem("email") as string
+          }
+
+          let temp = "";
+
+          if(this.labels.length !== 0){
+            temp = "&labels=" + JSON.stringify(this.labels);
+          }
+
+          value.save(`http://localhost:3333/api/models/create?userName=${user.name}&userEmail=${user.email}&name=${this.name}&created=${(new Date()).toString()}&type=${this.type}${temp}`).then((value:tf.io.SaveResult)=>{
+            this.notifications.add({type: "success",message: "Successfully uploaded model."});
+          }).catch((reason:any)=>{
+            this.notifications.add({type:"danger",message:"Failed to upload model"})
+          })
+          
+        }).catch((reason:any) => {
+          this.notifications.add({type: "danger", message: "Failed to load model."})
+        })
+      }break;
+      case 2:{
+        tf.loadLayersModel(`localstorage://${this.path}`).then((value:tf.LayersModel) => {
+          const user:user = {
+            name: sessionStorage.getItem("name") as string,
+            email: sessionStorage.getItem("email") as string
+          }
+
+          let temp = "";
+
+          if(this.labels.length !== 0){
+            temp = "&labels=" + JSON.stringify(this.labels);
+          }
+
+          value.save(`http://localhost:3333/api/models/create?userName=${user.name}&userEmail=${user.email}&name=${this.name}&created=${(new Date()).toString()}&type=${this.type}${temp}`).then((value:tf.io.SaveResult)=>{
+            this.notifications.add({type: "success",message: "Successfully uploaded model."});
+          }).catch((reason:any)=>{
+            this.notifications.add({type:"danger",message:"Failed to upload model"})
+          })
+          
+          
+        }).catch((reason:any) => {
+          this.notifications.add({type: "danger", message: "Failed to load model."})
+        })
+      }break;
+    }
   }
 }
