c8ebf8de9fc48cda627bd556c0fb53fdf98f5658
NasiphiM <u18074074@tuks.co.za>
2022-08-19 23:24:53 +0200

    💄 (editor) Help functionality

--- libs/client-lib/src/lib/editor/editor-help/editor-help.component.ts
+++ libs/client-lib/src/lib/editor/editor-help/editor-help.component.ts
@@ -9,30 +9,27 @@ export class EditorHelpComponent {
   isShown = {
     dataType : false,
     declaration : false,
-    arithmetic : false,
-    Operation : false 
+    arithmetic : false
   }
 
   showType():void{
     this.isShown.dataType = true;
     this.isShown.declaration = false;
     this.isShown.arithmetic = false;
-    this.isShown.Operation = false;
+    
   }
 
   showDecl(): void{
     this.isShown.declaration = true;
     this.isShown.dataType = false;
     this.isShown.arithmetic = false;
-    this.isShown.Operation = false;
+    
   }
 
-  showOp(): void{
-    this.isShown.Operation = true;
+  showArithmetic(): void{
+    this.isShown.arithmetic = true;
     this.isShown.declaration = false;
     this.isShown.dataType = false;
-    this.isShown.arithmetic = false;
-    
   }
 
 
