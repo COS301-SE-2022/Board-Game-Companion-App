54bf0b1bad08db12a45a440b2cc65430910e7976
NasiphiM <u18074074@tuks.co.za>
2022-08-20 13:51:38 +0200

    💄 (editor) : Help page

--- libs/client-lib/src/lib/editor/editor-help/editor-help.component.ts
+++ libs/client-lib/src/lib/editor/editor-help/editor-help.component.ts
@@ -9,13 +9,17 @@ export class EditorHelpComponent {
   isShown = {
     dataType : false,
     declaration : false,
-    arithmetic : false
+    arithmetic : false,
+    logical :false,
+    cond:false 
   }
 
   showType():void{
     this.isShown.dataType = true;
     this.isShown.declaration = false;
     this.isShown.arithmetic = false;
+    this.isShown.logical =false; 
+    this.isShown.cond = false;
     
   }
 
@@ -23,11 +27,31 @@ export class EditorHelpComponent {
     this.isShown.declaration = true;
     this.isShown.dataType = false;
     this.isShown.arithmetic = false;
+    this.isShown.logical =false; 
+    this.isShown.cond = false;
     
   }
 
   showArithmetic(): void{
     this.isShown.arithmetic = true;
+    this.isShown.declaration = false;
+    this.isShown.dataType = false;
+    this.isShown.logical =false; 
+    this.isShown.cond = false;
+  }
+
+  showLogical():void{
+    this.isShown.logical =true; 
+    this.isShown.arithmetic = false;
+    this.isShown.declaration = false;
+    this.isShown.dataType = false;
+    this.isShown.cond = false;
+  }
+
+  showCond():void {
+    this.isShown.cond = true;
+    this.isShown.logical =false; 
+    this.isShown.arithmetic = false;
     this.isShown.declaration = false;
     this.isShown.dataType = false;
   }
