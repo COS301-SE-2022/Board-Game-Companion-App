138bf9ff4b8d9c11f40500cf96d3fed7d84aedb6
MR Marsden <u18080368@tuks.co.za>
2022-08-21 18:20:04 +0200

    :lipstick:(VDSL): Addition of Visual Elements
    Addition of Visual Elements to sidebar.

--- libs/client-lib/src/lib/editor/editor-visuals/editor-visuals.component.ts
+++ libs/client-lib/src/lib/editor/editor-visuals/editor-visuals.component.ts
@@ -6,6 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editor-visuals.component.scss'],
 })
 export class EditorVisualsComponent implements OnInit {
+
+  origin = [
+    {title: 'Create',  class: 'visualC', pos: 0},
+    {title: 'Set',  class: 'visualS', pos: 0},
+    {title: 'Input',  class: 'visualIn', pos: 0},
+    {title: 'Output', class: 'visualO', pos: 0},
+    {title: 'Call', class: 'visualM', pos: 0},
+    {title: 'If', class: 'visualIf', pos: 0},
+    {title: 'Return', class: 'visualR', pos: 0},
+    {title: 'For', class: 'visualF', pos: 0},
+    {title: 'While', class: 'visualW', pos: 0},
+    {title: 'doWhile', class: 'visualD', pos: 0}
+  ];
   constructor() {}
 
   ngOnInit(): void {}
