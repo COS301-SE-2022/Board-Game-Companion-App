9bc81448a666eeba31e6ed0538bd47becf2da3fa
QabaNjabulo <u19062665@tuks.co.za>
2022-08-22 19:12:45 +0200

    🚧 added new components

---
+++ libs/client-lib/src/lib/scripts/own-scripts-page/own-scripts-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'board-game-companion-app-own-scripts-page',
+  templateUrl: './own-scripts-page.component.html',
+  styleUrls: ['./own-scripts-page.component.css'],
+})
+export class OwnScriptsPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- libs/client-lib/src/lib/scripts/scripts.module.ts
+++ libs/client-lib/src/lib/scripts/scripts.module.ts
@@ -3,20 +3,24 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ScriptsComponent } from './scripts/scripts.component';
 import { ToolBarComponent } from './tool-bar/tool-bar.component';
-import { PopoverModule } from "ngx-smart-popover";
+import { PopoverModule } from 'ngx-smart-popover';
 import { CreateScriptComponent } from './create-script/create-scripts.component';
 import { FormsModule } from '@angular/forms';
 import { UpdateScriptComponent } from './update-script/update-scripts.component';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { NbContextMenuModule } from '@nebular/theme';
-import { SharedModule } from '../shared/shared.module'
+import { SharedModule } from '../shared/shared.module';
+import { DownloadsPageComponent } from './downloads-page/downloads-page.component';
+import { OwnScriptsPageComponent } from './own-scripts-page/own-scripts-page.component';
 
 @NgModule({
   declarations: [
     ScriptsComponent,
     ToolBarComponent,
     CreateScriptComponent,
-    UpdateScriptComponent
+    UpdateScriptComponent,
+    DownloadsPageComponent,
+    OwnScriptsPageComponent,
   ],
   imports: [
     NbContextMenuModule,
@@ -25,7 +29,7 @@ import { SharedModule } from '../shared/shared.module'
     FormsModule,
     NgxPaginationModule,
     SharedModule,
-    RouterModule.forChild([{path:'',component:ScriptsComponent}])
-  ]
+    RouterModule.forChild([{ path: '', component: ScriptsComponent }]),
+  ],
 })
-export class ScriptsModule { }
+export class ScriptsModule {}
