a544d6f7ef312d14a92573d64235c7cde6c7a2f2
QabaNjabulo <u19062665@tuks.co.za>
2022-08-23 17:19:02 +0200

    🚧 own-scripts page added

--- libs/client-lib/src/lib/scripts/own-scripts-page/own-scripts-page.component.ts
+++ libs/client-lib/src/lib/scripts/own-scripts-page/own-scripts-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'board-game-companion-app-own-scripts-page',
   templateUrl: './own-scripts-page.component.html',
-  styleUrls: ['./own-scripts-page.component.css'],
+  styleUrls: ['./own-scripts-page.component.scss'],
 })
 export class OwnScriptsPageComponent implements OnInit {
   constructor() {}
