e04a2a64184a6dc03899ea874755f8a7bee63b61
Joseph-Makgopa <u18166793@tuks.co.za>
2022-08-23 12:51:00 +0200

    (scripts):deleted scripts with old schema from the database

--- libs/client-lib/src/lib/editor/editor-body/editor-body.component.ts
+++ libs/client-lib/src/lib/editor/editor-body/editor-body.component.ts
@@ -196,7 +196,9 @@ export class EditorBodyComponent implements OnInit,OnDestroy{
           }else{
             this.changesTracker.emit(0);
           }
+          console.log()
           this.newMessageEvent.emit(value.message);
+          console.log(value);
         },
         error:(e)=>{
           //console.log(e);
