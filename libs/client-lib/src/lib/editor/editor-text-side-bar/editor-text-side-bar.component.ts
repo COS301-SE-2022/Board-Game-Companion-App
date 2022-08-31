c9f380e282c50d7a4d68541b6c11178ca51ade4a
Joseph-Makgopa <u18166793@tuks.co.za>
2022-08-31 16:22:15 +0200

    (editor-models):added functionality to load the tensorflow models for a script

--- libs/client-lib/src/lib/editor/editor-text-side-bar/editor-text-side-bar.component.ts
+++ libs/client-lib/src/lib/editor/editor-text-side-bar/editor-text-side-bar.component.ts
@@ -1,8 +1,10 @@
-import { Component, Input, Output, EventEmitter, OnInit, OnChanges } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, OnChanges, ViewChild } from '@angular/core';
 import { script, empty } from '../../shared/models/script';
 import { selection } from '../../shared/models/selection';
 import { ModelsService } from '../../shared/services/models/models.service';
 import { StorageService } from '../../shared/services/storage/storage.service';
+import { NotificationComponent } from '../../shared/components/notification/notification.component';
+import * as tf from '@tensorflow/tfjs'
 
 @Component({
   selector: 'board-game-companion-app-editor-text-side-bar',
@@ -16,6 +18,7 @@ export class EditorTextSideBarComponent implements OnInit,OnChanges{
   @Input() current:script = empty;
   @Output() selectionEvent = new EventEmitter<selection>();
   @Output() removeEvent = new EventEmitter<selection>();
+  @ViewChild(NotificationComponent,{static:true}) notifications: NotificationComponent = new NotificationComponent();
 
   constructor(private readonly modelService:ModelsService,private readonly storageService:StorageService){}
 
@@ -37,11 +40,25 @@ export class EditorTextSideBarComponent implements OnInit,OnChanges{
   }
 
   getModels(): void{
+
     this.modelService.getModels(this.current.models).subscribe({
       next:(value:any) => {
         this.models = value.map((value:any) => value.name);
-        this.models.forEach((value:any) => {
 
+        this.storageService.clear("networks").then((res:string) => {
+          
+          value.forEach((network:any) => {
+            this.storageService.insert("networks",network).then(async(res:string)=>{
+              const model = await tf.loadLayersModel(network.model.location);
+              
+              await model.save(`indexeddb://${network.name}`);
+            }).catch(() =>{
+              this.notifications.add({type:"danger",message:`Failed to load model ${network.name}`})
+            })
+          })
+
+        }).catch(() => {
+          this.notifications.add({type:"danger",message:'Failed to clear storage for storing models.'})
         })
       },
       error:(err) => {
@@ -49,4 +66,5 @@ export class EditorTextSideBarComponent implements OnInit,OnChanges{
       }
     })
   }
+
 }
