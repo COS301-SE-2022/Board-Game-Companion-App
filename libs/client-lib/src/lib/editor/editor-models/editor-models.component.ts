35a1810d3e025062f667e4a3b1c94aeac5d876c7
Joseph-Makgopa <u18166793@tuks.co.za>
2022-08-31 10:44:56 +0200

    (editor-models):implemented ui for adding model to script

--- libs/client-lib/src/lib/editor/editor-models/editor-models.component.ts
+++ libs/client-lib/src/lib/editor/editor-models/editor-models.component.ts
@@ -1,13 +1,28 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { ModelsService } from '../../shared/services/models/models.service';
 
 @Component({
   selector: 'board-game-companion-app-editor-models',
   templateUrl: './editor-models.component.html',
   styleUrls: ['./editor-models.component.scss'],
 })
-export class EditorModelsComponent {
- 
+export class EditorModelsComponent implements OnInit{
+  networks:any = [];
+
+  constructor(private readonly modelsService:ModelsService){}
+
   apply(){
     console.log("void")
+  }
+
+  ngOnInit(): void {
+    this.modelsService.getAll().subscribe({
+      next:(value: any) => {
+        this.networks = value;
+      },
+      error:(err) => {
+        console.log(err)
+      }
+    })    
   }
 }
