1c6d16ef3ddd13073168880ead5c05c7dcf9fd33
MR Marsden <u18080368@tuks.co.za>
2022-09-05 01:59:38 +0200

    ✨(VDSL): Player Containers Collapse
    Made player containers collapsible for readability.

--- libs/client-lib/src/lib/editor/editor-body-visual/player-template.ts
+++ libs/client-lib/src/lib/editor/editor-body-visual/player-template.ts
@@ -1,34 +1,37 @@
 import { Component, Input} from "@angular/core";
-import { ElementTemplateComponent } from "./element-template";
 
 @Component({
     selector: 'board-game-companion-app-player-template',
     styleUrls:['./editor-body-visual.component.scss'],
     template: `
         <div id = "player" class = "boardContainers">
-            <div class = "title text-2xl font-bold ml-4 mt-2 mb-4">
-                Player <input>
-            </div>
-            <div class = "ActionConditionPairs" *ngFor = "item of Actions let i = index">
-                <div id = "action" class = "playerContainers">
-                    <div class = "title text-xl font-bold ml-4 mt-2">
-                        Action <input> <input>
+            <details open>
+                <summary class = "list-none flex flex-wrap items-center cursor-pointer">
+                    <h3 class = "title text-2xl font-bold ml-4 mt-2 mb-4">
+                        Player <input> 
+                    </h3>
+                </summary>
+                <div class = "ActionConditionPairs" *ngFor = "item of Actions let i = index">
+                    <div id = "action" class = "playerContainers">
+                        <div class = "title text-xl font-bold ml-4 mt-2">
+                            Action <input> <input>
+                        </div>
+                        <board-game-companion-app-element-template class="wrapper" dragula="COPYABLE" [(dragulaModel)]="Actions[i]" [dest] = "Actions[i]" [dests] = "PlayerLoops" [methods] = "methods"></board-game-companion-app-element-template>
                     </div>
-                    <board-game-companion-app-element-template class="wrapper" dragula="COPYABLE" [(dragulaModel)]="Actions[i]" [dest] = "Actions[i]" [dests] = "PlayerLoops" [methods] = "methods"></board-game-companion-app-element-template>
-                </div>
-                <div id = "condition" class = "playerContainers">
-                    <div class = "title text-xl font-bold ml-4 mt-2">
-                        Condition <input>
+                    <div id = "condition" class = "playerContainers">
+                        <div class = "title text-xl font-bold ml-4 mt-2">
+                            Condition <input>
+                        </div>
+                        <board-game-companion-app-element-template class="wrapper" dragula="COPYABLE" [(dragulaModel)]="Conditions[i]" [dest] = "Conditions[i]" [dests] = "PlayerLoops" [methods] = "methods"></board-game-companion-app-element-template>
                     </div>
-                    <board-game-companion-app-element-template class="wrapper" dragula="COPYABLE" [(dragulaModel)]="Conditions[i]" [dest] = "Conditions[i]" [dests] = "PlayerLoops" [methods] = "methods"></board-game-companion-app-element-template>
                 </div>
-            </div>
-            <div id = "turn" class = "playerContainers">
-                <div class = "title text-xl font-bold ml-4 mt-2">
-                    Turn
+                <div id = "turn" class = "playerContainers">
+                    <div class = "title text-xl font-bold ml-4 mt-2">
+                        Turn
+                    </div>
+                    <board-game-companion-app-element-template class="wrapper" dragula="COPYABLE" [(dragulaModel)]="Turn" [dest] = "Turn" [dests] = "PlayerLoops" [methods] = "methods"></board-game-companion-app-element-template>
                 </div>
-                <board-game-companion-app-element-template class="wrapper" dragula="COPYABLE" [(dragulaModel)]="Turn" [dest] = "Turn" [dests] = "PlayerLoops" [methods] = "methods"></board-game-companion-app-element-template>
-            </div>
+            </details>
         </div>
     `
 })
