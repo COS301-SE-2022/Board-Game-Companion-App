4b1beb83e17864f19d14e43e6d570be45f4063b6
Joseph-Makgopa <u18166793@tuks.co.za>
2022-09-04 18:49:26 +0200

    (automata-script):added function to retrieve old versions in api service

--- libs/api-lib/src/lib/services/automata/automata.service.ts
+++ libs/api-lib/src/lib/services/automata/automata.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { AutomataScript, AutomataScriptDocument } from '../../schemas/automata-script.schema';
+import { OldScript, OldScriptDocument } from '../../schemas/old-script.schema';
 import { user } from '../../models/general/user';
 import fs = require('fs');
 import { ModelsService } from '../models/models.service';
@@ -9,10 +10,15 @@ import { ModelsService } from '../models/models.service';
 @Injectable()
 export class AutomataService {
     constructor(@InjectModel(AutomataScript.name) private automataModel: Model<AutomataScriptDocument>,
+                @InjectModel(OldScript.name) private oldModel: Model<OldScriptDocument>,
                 private readonly modelService:ModelsService){}
 
     async getAll():Promise<AutomataScript[]>{
         return this.automataModel.find({});
+    }
+
+    async getOldVersions(name:string,author:user):Promise<OldScript[]>{
+        return this.oldModel.find({"name":name,"author.name":author.name,"author.email":author.email});
     }
 
     async getAutomataScript(name:string,author:user):Promise<AutomataScriptDocument>{
