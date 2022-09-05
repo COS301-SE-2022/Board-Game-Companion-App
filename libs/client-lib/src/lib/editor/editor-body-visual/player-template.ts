5d9cc78f761f1c955cecee5d74c8e183fd644dca
MR Marsden <u18080368@tuks.co.za>
2022-09-05 03:06:54 +0200

    ✨(VDSL): Add Action button, Containers inside Player collapsible
    Added action button to add more actions to a player and made action, condition and turn containers collapsible.

--- libs/client-lib/src/lib/editor/editor-body-visual/player-template.ts
+++ libs/client-lib/src/lib/editor/editor-body-visual/player-template.ts
@@ -7,29 +7,42 @@ import { Component, Input} from "@angular/core";
         <div id = "player" class = "boardContainers">
             <details open>
                 <summary class = "list-none flex flex-wrap items-center cursor-pointer">
-                    <h3 class = "title text-2xl font-bold ml-4 mt-2 mb-4">
+                    <div class = "title text-2xl font-bold ml-4 mt-2 mb-4">
                         Player <input> 
-                    </h3>
+                    </div>
                 </summary>
+                <button (click)="addAction()" id = "addAction">Add Action</button>
                 <div class = "ActionConditionPairs" *ngFor = "item of Actions let i = index">
                     <div id = "action" class = "playerContainers">
-                        <div class = "title text-xl font-bold ml-4 mt-2">
-                            Action <input> <input>
-                        </div>
-                        <board-game-companion-app-element-template class="wrapper" dragula="COPYABLE" [(dragulaModel)]="Actions[i]" [dest] = "Actions[i]" [dests] = "PlayerLoops" [methods] = "methods"></board-game-companion-app-element-template>
+                        <details open>
+                            <summary class = "list-none flex flex-wrap items-center cursor-pointer">
+                                <div class = "title text-xl font-bold ml-4 mt-2">
+                                    Action <input> <input>
+                                </div>
+                            </summary>
+                            <board-game-companion-app-element-template class="wrapper" dragula="COPYABLE" [(dragulaModel)]="Actions[i]" [dest] = "Actions[i]" [dests] = "PlayerLoops" [methods] = "methods"></board-game-companion-app-element-template>
+                        </details>
                     </div>
                     <div id = "condition" class = "playerContainers">
-                        <div class = "title text-xl font-bold ml-4 mt-2">
-                            Condition <input>
-                        </div>
-                        <board-game-companion-app-element-template class="wrapper" dragula="COPYABLE" [(dragulaModel)]="Conditions[i]" [dest] = "Conditions[i]" [dests] = "PlayerLoops" [methods] = "methods"></board-game-companion-app-element-template>
+                        <details open>
+                            <summary class = "list-none flex flex-wrap items-center cursor-pointer">
+                                <div class = "title text-xl font-bold ml-4 mt-2">
+                                    Condition <input>
+                                </div>
+                            </summary>
+                            <board-game-companion-app-element-template class="wrapper" dragula="COPYABLE" [(dragulaModel)]="Conditions[i]" [dest] = "Conditions[i]" [dests] = "PlayerLoops" [methods] = "methods"></board-game-companion-app-element-template>
+                        </details>
                     </div>
                 </div>
                 <div id = "turn" class = "playerContainers">
-                    <div class = "title text-xl font-bold ml-4 mt-2">
-                        Turn
-                    </div>
-                    <board-game-companion-app-element-template class="wrapper" dragula="COPYABLE" [(dragulaModel)]="Turn" [dest] = "Turn" [dests] = "PlayerLoops" [methods] = "methods"></board-game-companion-app-element-template>
+                    <details open>
+                        <summary class = "list-none flex flex-wrap items-center cursor-pointer">
+                            <div class = "title text-xl font-bold ml-4 mt-2">
+                                Turn
+                            </div>
+                        </summary>
+                        <board-game-companion-app-element-template class="wrapper" dragula="COPYABLE" [(dragulaModel)]="Turn" [dest] = "Turn" [dests] = "PlayerLoops" [methods] = "methods"></board-game-companion-app-element-template>
+                    </details>
                 </div>
             </details>
         </div>
@@ -46,5 +59,10 @@ export class PlayerTemplateComponent{
         {name: 'addPieceToTile', arguments: 2},
         {name: 'addToArr', arguments: 2}
       ]
+
+    addAction(){
+        this.Actions.push([{title: '', class: '' , id: '', pos: 0}])
+        this.Conditions.push([{title: '', class: '' , id: '', pos: 0}])
+    }
     
 }
