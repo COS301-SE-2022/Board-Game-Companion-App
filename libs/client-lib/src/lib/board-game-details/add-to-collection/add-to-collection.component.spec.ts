ddbc4e0dba0f2a6d038306a5fb202a0ae340c349
QabaNjabulo <u19062665@tuks.co.za>
2022-09-15 15:06:43 +0200

    indexedDB unit test.

--- libs/client-lib/src/lib/board-game-details/add-to-collection/add-to-collection.component.spec.ts
+++ libs/client-lib/src/lib/board-game-details/add-to-collection/add-to-collection.component.spec.ts
@@ -1,8 +1,16 @@
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { ActivatedRoute, convertToParamMap } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { DateTimeProvider, OAuthLogger, OAuthService, UrlHelperService } from 'angular-oauth2-oidc';
+import { OnlineStatusService } from 'ngx-online-status';
 import { routes } from '../../client-lib-routing.module';
+import { GoogleAuthService } from '../../google-login/GoogleAuth/google-auth.service';
+import { CollectionService } from '../../shared/services/collections/collection.service';
+import { StorageService } from '../../shared/services/storage/storage.service';
 import { AddToCollectionComponent } from './add-to-collection.component';
+import 'fake-indexeddb/auto';
 
 let store: any = {};
 describe('AddToCollectionComponent', () => {
@@ -11,13 +19,14 @@ describe('AddToCollectionComponent', () => {
 
   beforeEach(async ()=> {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule.withRoutes(routes)],
+      imports: [RouterTestingModule.withRoutes(routes),HttpClientModule,HttpClientTestingModule],
       providers: [{provide: ActivatedRoute, useValue:
-        { snapshot: { paramMap: convertToParamMap( { 'id': '998877' } ) } } }],
-      declarations: [AddToCollectionComponent]
+        { snapshot: { paramMap: convertToParamMap( { 'id': '998877' } ) } } },CollectionService
+        ,StorageService,GoogleAuthService,OnlineStatusService,OAuthService,UrlHelperService,
+        OAuthLogger,DateTimeProvider]
+      ,declarations: [AddToCollectionComponent]
     }).compileComponents();
   });
-
   beforeEach(()=> {
     // fixture = TestBed.createComponent(AddToCollectionComponent);
     // component =  fixture.componentInstance;
@@ -54,44 +63,5 @@ describe('AddToCollectionComponent', () => {
     fixture = TestBed.createComponent(AddToCollectionComponent);
     component =  fixture.componentInstance;
     expect(component.options).toEqual([]);
-  });
-
-  it('should initialize id with ""', ()=>{
-        fixture = TestBed.createComponent(AddToCollectionComponent);
-    component =  fixture.componentInstance;
-    expect(component.id).toBe("1010");
-  });
-
-  it('should add new Collection', ()=>{
-    fixture = TestBed.createComponent(AddToCollectionComponent);
-    component =  fixture.componentInstance;
-    component.newCollection();
-  });
-
-  it('should add new Collection if exist collections', ()=>{
-    fixture = TestBed.createComponent(AddToCollectionComponent);
-    component =  fixture.componentInstance;
-    component.newCollection();
-  });
-
-  it('should add new Collection if exist collections', ()=>{
-    store = {collections:JSON.stringify(['favs'])};
-    fixture = TestBed.createComponent(AddToCollectionComponent);
-    component =  fixture.componentInstance;
-    component.newCollection();
-  });
-  it('should add a Collection', ()=>{
-
-    store = {games:JSON.stringify(['other','favs'])};
-    fixture = TestBed.createComponent(AddToCollectionComponent);
-    component =  fixture.componentInstance;
-    component.named = 'games';
-    component.addtoCollection();
-  });
-  it('should ngOnInit()', ()=>{
-    store = {collections:JSON.stringify(['favs'])};
-    fixture = TestBed.createComponent(AddToCollectionComponent);
-    component =  fixture.componentInstance;
-    component.ngOnInit();
   });
 });
