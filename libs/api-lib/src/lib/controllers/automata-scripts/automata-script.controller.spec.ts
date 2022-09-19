5c22d5087b4875fea4444a0c7860d6fd0f7be3d6
NasiphiM <u18074074@tuks.co.za>
2022-09-19 14:34:44 +0200

    ✅(unit test):collections.controller

--- libs/api-lib/src/lib/controllers/automata-scripts/automata-script.controller.spec.ts
+++ libs/api-lib/src/lib/controllers/automata-scripts/automata-script.controller.spec.ts
@@ -1,14 +1,15 @@
-import { Test, TestingModule } from "@nestjs/testing"
-import { AutomataService } from "../../services/automata/automata.service"
-import { RatingService } from "../../services/ratings/rating.service"
-import { ApiAutomataScriptController } from "../automata-scripts/automata-script.controller"
+import { Test, TestingModule } from "@nestjs/testing";
+//import { createMock } from '@golevelup/ts-jest';
+import { AutomataService } from "../../services/automata/automata.service";
+import { RatingService } from "../../services/ratings/rating.service";
+import { ApiAutomataScriptController } from "../automata-scripts/automata-script.controller";
 import { ModelsService } from "../../services/models/models.service";
-import { MongoDbStorageService } from "../../services/mongodb-storage/mongodb-storage.service"
-import { AutomataScript, AutomataScriptDocument } from "../../schemas/automata-script.schema"
-import { OldScript, OldScriptDocument } from  "../../schemas/old-script.schema"
-import { DownloadScript, DownloadScriptDocument } from "../../schemas/download-script.schema"
+import { MongoDbStorageService } from "../../services/mongodb-storage/mongodb-storage.service";
+import { AutomataScript, AutomataScriptDocument } from "../../schemas/automata-script.schema";
+import { OldScript, OldScriptDocument } from  "../../schemas/old-script.schema";
+import { DownloadScript, DownloadScriptDocument } from "../../schemas/download-script.schema";
 import mongoose, { Model, Connection, connect } from "mongoose";
-import { ApiLibModule } from "../../api-lib.module";
+
 import { user } from "../../models/general/user";
 import { version } from "../../models/general/version";
 
@@ -21,7 +22,7 @@ describe('AutomataScriptController',()=>{
 
   beforeEach(async () => {
     const moduleRef: TestingModule = await Test.createTestingModule({
-      imports: [ApiLibModule],
+      //imports: [ApiLibModule],
       controllers: [ApiAutomataScriptController],
       providers: [
         //RatingService 
@@ -36,10 +37,12 @@ describe('AutomataScriptController',()=>{
         Promise.resolve({
           user: user,
           script: script,
-          value: 15
+          value: 2
         })),
-        average: jest.fn().mockResolvedValue(50),
-        countRating: jest.fn().mockReturnValue(18)
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        average: jest.fn().mockImplementation((script:string) => Promise.resolve(50)),
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        countRating: jest.fn().mockImplementation((script:string) => Promise.resolve(18))
       }},
       //AutomataService
       { provide:AutomataService,useValue:{
@@ -178,7 +181,7 @@ describe('AutomataScriptController',()=>{
           downloads: 5,
           lastDownload: new Date("10-09-21"),
           export: false,
-          comments: [{commentId, ref:"David"}],
+          comments: [{type: commentId, ref:"David"}],
           source: {name:"Source3", key:"key2323", location:"location254"}
       })),
       getScriptById: jest.fn().mockImplementation((id:string) => 
@@ -261,142 +264,213 @@ describe('AutomataScriptController',()=>{
 
   });
 
-  // describe('retrieve-all' , () => {
-  //   it('should get all of the scripts', async () => {
-  //     const result = new Promise<AutomataScript[]>((resolve, reject) => { 
-  //       resolve([{ 
-  //         name: "Base Script", 
+  describe('getOldVersions', () => {
+    it ('should get older versions of the script', () =>{
+    
+     expect(apiAutomataScriptController.getOldVersions("a string of idLists")).resolves.toEqual({
+      name: "Jake",
+      author: {name:"David", email:"Davidjones@gmail.com"},
+      boardgame: "monopoly",
+      description: "This is a monopoly board game", 
+      version: {major:8, minor:8, patch:8},
+      size: 32, 
+      icon:{name:"thisIcon2",key:"icon890",location:"Iconlocation2"},
+      build:{name:"thisBuild",key:"key1234",location:"thislocation2"},
+      models: "a string of idLists",
+      iconSize:10, //baseScript
+      dateReleased: new Date("11-02-16"),
+      downloads: 5,
+      lastDownload: new Date("10-09-21"),
+      export: false,
+      comments: [],
+      source: {name:"Source3", key:"key2323", location:"location254"}
+       })
+     })
+ 
+   });
 
-  //         author: {name:"John",email:"JohnSmith@gmail.com"},
-          
-  //         boardgame: "Snakes and Ladders",
-      
-  //         description: "This is a base script",
-          
-  //         version: {major:0, minor: 0, patch: 0}, 
-          
-  //         size: 260,
-      
-  //         icon: {name:"iconName", key:"key123", location:"iconsFolder"},
-          
-  //         build: {name:"buildName", key:"buildkey123", location:"buildFolder"},
-          
-  //         models: ["myModel"],
-      
-  //         iconSize: 20,
-  //         //end base script
+   describe('getAll', () => {
+    it ('should return all scripts', () =>{
+    
+     expect(apiAutomataScriptController.getAll()).resolves.toEqual([
+      {
+        name: "Script1",
+        author: {name:"Jack", email:"jackdaniels@gmail.com"},
+        boardgame: "TicTacToe",
+        description: "This is a tictactoe board game", 
+        version: {major:8, minor:8, patch:8},
+        size: 32, 
+        icon:{name:"thisIcon",key:"icon567",location:"Iconlocation"},
+        build:{name:"thisBuild",key:"key1234",location:"thislocation"},
+        models:["thisisaModel", "thisisanothermodel"],
+        iconSize:10, //base script 
+        previous: ["Script1.0"],
+        link:"www.thisisalink.com",
+        dateReleased: new Date("18-06-17"), 
+        downloads: 3, 
+        lastDownload: new Date("17-07-22"),
+        export: true,
+        comments: [],
+        source: {name:"fileName", key:"key555", location:"filelokcation"}
+      },
+      {
+        name: "Script2",
+        author: {name:"David", email:"Davidjones@gmail.com"},
+        boardgame: "monopoly",
+        description: "This is a monopoly board game", 
+        version: {major:8, minor:8, patch:8},
+        size: 32, 
+        icon:{name:"thisIcon2",key:"icon890",location:"Iconlocation2"},
+        build:{name:"thisBuild",key:"key1234",location:"thislocation2"},
+        models:["thisisaModel2", "thisisanothermodel2"],
+        iconSize:10, //base script 
+        previous: ["Script1.1.0"],
+        link:"www.thisisalink2.com",
+        dateReleased: new Date("24-03-17"), 
+        downloads: 3, 
+        lastDownload: new Date("25-07-22"),
+        export: true,
+        comments: [],
+        source: {name:"fileName2", key:"key333", location:"filelocation2"}
+      }
+    ])
+     })
+ 
+   });
 
-  //         previous:["This", "Then", "That"],
-            
-  //         link: "www.thisisalink.com",
+   describe('getById', () => {
+    it ('should return a script with the provided id', () =>{
+    
+     expect(apiAutomataScriptController.getById("some string id")).resolves.toEqual([
+      {
+        name: "some string id",
+        author: {name:"David", email:"Davidjones@gmail.com"},
+        boardgame: "monopoly",
+        description: "This is a monopoly board game", 
+        version: {major:8, minor:8, patch:8},
+        size: 32, 
+        icon:{name:"thisIcon2",key:"icon890",location:"Iconlocation2"},
+        build:{name:"thisBuild",key:"key1234",location:"thislocation2"},
+        models:["thisisaModel2", "thisisanothermodel2"],
+        iconSize:10, //baseScript
+        dateReleased: new Date("11-02-16"),
+        downloads: 5,
+        lastDownload: new Date("10-09-21"),
+        export: false,
+        comments: [],
+        source: {name:"Source3", key:"key2323", location:"location254"}
+      }
+    ])
+     })
+ 
+   });
 
-  //         dateReleased: new Date("28-01-19"),
+   describe('addComment', () => {
+    it ('should return a script with the provided id', () =>{
+    
+     expect(apiAutomataScriptController.addComment("some script id", "some comment id")).resolves.toEqual(
+      {
+        name: "some script id",
+        author: {name:"David", email:"Davidjones@gmail.com"},
+        boardgame: "monopoly",
+        description: "This is a monopoly board game", 
+        version: {major:8, minor:8, patch:8},
+        size: 32, 
+        icon:{name:"thisIcon2",key:"icon890",location:"Iconlocation2"},
+        build:{name:"thisBuild",key:"key1234",location:"thislocation2"},
+        models:["thisisaModel2", "thisisanothermodel2"],
+        iconSize:10, //baseScript
+        dateReleased: new Date("11-02-16"),
+        downloads: 5,
+        lastDownload: new Date("10-09-21"),
+        export: false,
+        comments: [{type:"some comment id", ref:"David"}],
+        source: {name:"Source3", key:"key2323", location:"location254"}
+      })
+     })
+ 
+   });
+ 
+   describe('createUserRating', () => {
+    it ('should provide rating of the script', () =>{
+      const newUser: user ={
+        name: "Beyonce",
+        email: "Beyonce1234@gmail.com"
+      };
+     expect(apiAutomataScriptController.createUserRating(newUser, "script id", 3)).resolves.toEqual(
+      {
+        user: newUser,
+        script: "script id",
+        value: 3
+      })
+     })
+ 
+   });
 
-  //         downloads: 10,
-          
-  //         lastDownload: new Date("28-07-19"),
+   describe('retrieveUserRating', () => {
+    it ('should get rating of the script from specific user', () =>{
+      const newUser: user ={
+        name: "Beyonce",
+        email: "Beyonce1234@gmail.com"
+      };
+     expect(apiAutomataScriptController.retrieveUserRating(newUser.name, newUser.email, "some script Id")).resolves.toEqual(
+      {
+        user: {name: newUser.name, email: newUser.email},
+        script: "some script Id",
+        value: 2
+      })
+     })
+ 
+   });
 
-  //         export: true,
+   describe('countRating', () => {
+    it ('should count the rating of script', () =>{
+      expect(apiAutomataScriptController.countRating("some script Id")).resolves.toEqual(18)
+    })
+ 
+   });
 
-  //         comments: [],
+  describe('averageRating', () => {
+    it ('should display the average rating of a script', () =>{
+      expect(apiAutomataScriptController.averageRating("some script Id")).resolves.toEqual(50)
+    })
+ 
+  });
 
-  //         source: {name:"sourceFile", key:"1234", location:"desktop"}
-  //       },
-  //       {
-  //         name: "Base Script2", 
-
-  //         author: {name:"Sam",email:"SamSmith@gmail.com"},
-          
-  //         boardgame: "Chess",
-      
-  //         description: "This is a base script",
-          
-  //         version: {major:0, minor: 0, patch: 0}, 
-          
-  //         size: 260,
-      
-  //         icon: {name:"iconName", key:"key342", location:"iconsFolder"},
-          
-  //         build: {name:"buildName", key:"buildkey342", location:"buildFolder"},
-          
-  //         models: ["myModel"],
-      
-  //         iconSize: 25,
-
-  //         previous:["That", "Then", "This"],
-            
-  //         link: "www.thisisanotherlink.com",
-
-  //         dateReleased: new Date("08-08-18"),
-
-  //         downloads: 10,
-          
-  //         lastDownload: new Date("22-02-22"),
-
-  //         export: false,
-
-  //         comments: [],
-
-  //         source: {name:"sourceFile", key:"1234", location:"desktop"}
-  //       }])
-  //     });
-  //   jest.spyOn(automataService, 'retrieve-all').mockImplementation(()=> result);
-
-  //   expect(await apiAutomataScriptController.getAll()).toBe(await result);
-
-  //   })
-  // })
-
-  // describe('retrive-by-id', () => {
-  //   it('should retrive a script by the id', async () => {
-  //     const result = new Promise<AutomataScript>((resolve, reject) => {
-  //       resolve({
-  //         name: "Base Script", 
-
-  //         author: {name:"John",email:"JohnSmith@gmail.com"},
-          
-  //         boardgame: "Snakes and Ladders",
-      
-  //         description: "This is a base script",
-          
-  //         version: {major:0, minor: 0, patch: 0}, 
-          
-  //         size: 260,
-      
-  //         icon: {name:"iconName", key:"key123", location:"iconsFolder"},
-          
-  //         build: {name:"buildName", key:"buildkey123", location:"buildFolder"},
-          
-  //         models: ["myModel"],
-      
-  //         iconSize: 20,
-  //         //end of base script 
-         
-  //         previous: ["string1", "string2", "string3"],
-          
-  //         link: "www.thisisalink.com",
-          
-  //         dateReleased: new Date("15-04-15"),
-      
-  //         downloads: 15,
-      
-  //         lastDownload: new Date("30-02-19"),
-          
-  //         export: false,
-      
-  //         comments: [],
-      
-  //         source: {name:"sourceName",key:"sourcekey1234", location:"sourceLocation"}
-  //       })
-  //     });
-      
-  //     jest.spyOn(automataService, 'getScriptById').mockImplementation(()=> result);
-
-  //     expect(await apiAutomataScriptController.getById("4545")).toBe(await result);
-
-  //   });
-
-  // });
+  describe('checkForUpdatesForOne', () => {
+    it ('should check for updated scripts', () =>{
+      expect(apiAutomataScriptController.checkForUpdatesForOne("some script Id")).resolves.toEqual("This "+"some script Id"+" is found!")
+    })
+ 
+  });
+  
+  
+  describe('getByGame', () => {
+    it ('should check for updated scripts', () =>{
+      expect(apiAutomataScriptController.getByGame("some board game Id")).resolves.toEqual(
+        [{
+          name: "Peter",
+          author: {name:"Mark", email:"MarkDavids@gmail.com"},
+          boardgame: "some board game Id",
+          description: "This is a roots board game", 
+          version: {major:2, minor:8, patch:8},
+          size: 32, 
+          icon:{name:"thisIcon3",key:"icon80",location:"Iconlocation3"},
+          build:{name:"thisBuild",key:"key134",location:"thislocation3"},
+          models:["thisisaModel3", "thisisanothermodel3"],
+          iconSize:10, //base script 
+          previous: ["Script1.2.5"],
+          link:"www.thisisalink2.com",
+          dateReleased: new Date("18-02-16"), 
+          downloads: 8, 
+          lastDownload: new Date("20-05-22"),
+          export: true,
+          comments: [],
+          source: {name:"fileName3", key:"key313", location:"filelocation3"}
+      }])
+    })
+ 
+  });
 
 });
 
--- libs/api-lib/src/lib/controllers/board-collection/collections.controller.spec.ts
+++ libs/api-lib/src/lib/controllers/board-collection/collections.controller.spec.ts
@@ -1,5 +1,167 @@
-describe('Test editor',()=>{
-  it('testing testing',()=>{
-    expect("1").toBe("1");
-  })
+//import { createMock } from '@golevelup/ts-jest';
+import { Test, TestingModule } from "@nestjs/testing"
+import { user } from '../../models/general/user';
+import { CollectionsService } from '../../services/collection/collections.service';
+import { CollectionsController } from "../board-collection/collections.controller"
+
+jest.mock('../../services/collection/collections.service');
+
+describe('CollectionsController',()=>{
+  let service : CollectionsService; 
+  let controller: CollectionsController;
+
+  beforeEach(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      controllers: [CollectionsController], 
+      providers: [
+        {
+          provide: CollectionsService, 
+          useValue: {
+            create: jest.fn().mockImplementation((name:string,owner:user)=>
+              Promise.resolve({
+                owner: owner,
+                name: name,
+                boardgames: ["30 seconds" ,"roots", "monopoly"]
+              })),
+            alreadyExist: jest.fn().mockImplementation((owner:user,name:string)=>
+              Promise.resolve(true)),
+            getCollectionsByUser: jest.fn().mockImplementation((owner: user)=>
+              Promise.resolve({
+                owner: owner,
+                name: "Collection1",
+                boardgames: ["chess" ,"roots", "snakes and ladders"]
+              })
+            ),
+            getAllCollections: jest.fn().mockResolvedValue([
+              {owner: {name:"James", email:"jamesbond007@gmail.com"}, name: "Collection1", boardgames: ["chess" , "snakes and ladders"]},
+              {owner: {name:"micheal", email:"michealsmith@gmail.com"}, name: "Collection2", boardgames: ["checkers" ,"roots", "monopoly"]},
+              {owner: {name:"jennifer", email:"jenniferlopez@gmail.com"}, name: "Collection3", boardgames: ["boardgame1", "boardgame2", "boardgame4"]},
+            ]),
+            addBoardGame: jest.fn().mockImplementation((boardgame:string,name:string,owner:user)=>Promise.resolve(true)),
+            removeBoardGame: jest.fn().mockImplementation((boardgame:string,name:string,owner:user)=>Promise.resolve(2)),
+            removeCollectionById: jest.fn().mockImplementation((id:string)=>Promise.resolve(2)),
+            removeCollection: jest.fn().mockImplementation((owner:user,name:string)=> Promise.resolve(2)),
+            getScripts: jest.fn().mockImplementation((id:string)=>Promise.resolve({
+              name: id,
+              author: {name:"Mark", email:"MarkDavids@gmail.com"},
+              boardgame: "Chess",
+              description: "This is a roots board game", 
+              version: {major:2, minor:8, patch:8},
+              size: 32, 
+              icon:{name:"thisIcon3",key:"icon80",location:"Iconlocation3"},
+              build:{name:"thisBuild",key:"key134",location:"thislocation3"},
+              models:["thisisaModel3", "thisisanothermodel3"],
+              iconSize:10, //basescript
+              previous: ["Script1.2.5"],
+              link:"www.thisisalink2.com",
+              dateReleased: new Date("18-02-16"), 
+              downloads: 8, 
+              lastDownload: new Date("20-05-22"),
+              export: true,
+              comments: [],
+              source: {name:"fileName3", key:"key313", location:"filelocation3"}
+            }))
+          }
+        }
+      ]
+    }).compile();
+
+    controller = moduleRef.get<CollectionsController>(CollectionsController);
+    service =  moduleRef.get<CollectionsService>(CollectionsService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  describe('getCollectionsForUser', ()=>{
+    it('should get a collection for specific User', ()=>{
+      expect(controller.getCollectionsForUser("owner's Name", "owner's email")).resolves.toEqual({
+        owner: {name:"owner's Name", email:"owner's email"},
+        name: "Collection1",
+        boardgames: ["chess" ,"roots", "snakes and ladders"]
+      });
+    });
+  });
+
+  describe('createCollection', ()=>{
+    it('should create a collection', ()=>{
+      const newUser: user ={
+        name: "newUser",
+        email: "newUser@gmail.com"
+      };
+        expect(controller.createCollection("Collection3", newUser)).resolves.toEqual({
+          owner: newUser,
+          name: "Collection3",
+          boardgames: ["30 seconds" ,"roots", "monopoly"]  
+        });
+    });
+  });
+
+  describe('removeCollection', ()=>{
+    it ('should remove a collection', ()=>{
+      expect(controller.removeCollection("FaveCollection","James","JamesJennifer@gmail.com")).resolves.toEqual(2)
+    });
+  });
+
+  describe('removeBoardGame', ()=>{
+    it('should remove a board game', ()=>{
+      const newUser: user ={
+        name: "Jennifer",
+        email: "jenniferaustin@gmail.com"
+      };
+      expect(controller.removeBoardGame("30 seconds","Collection4",newUser.name,newUser.email)).resolves.toEqual(2)
+    });
+  });
+
+  describe('removeCollectionById', ()=>{
+    it('should remove a collection by its id', ()=>{
+      expect(controller.removeCollectionById("string ID")).resolves.toEqual(2)
+    });
+  });
+
+  describe('addGameToCollection', ()=>{
+    it('should add a board game to a collection', ()=>{
+      const newUser: user ={
+        name: "Jennifer",
+        email: "jenniferaustin@gmail.com"
+      }
+      expect(controller.addGameToCollection(newUser,"ChessCollection","Chess")).resolves.toEqual(true)
+    });
+  }); 
+
+  describe('getScripts', ()=>{
+    it('should get all scripts', ()=>{
+      expect(controller.getScripts("ScriptId")).resolves.toEqual({
+        name: "ScriptId",
+        author: {name:"Mark", email:"MarkDavids@gmail.com"},
+        boardgame: "Chess",
+        description: "This is a roots board game", 
+        version: {major:2, minor:8, patch:8},
+        size: 32, 
+        icon:{name:"thisIcon3",key:"icon80",location:"Iconlocation3"},
+        build:{name:"thisBuild",key:"key134",location:"thislocation3"},
+        models:["thisisaModel3", "thisisanothermodel3"],
+        iconSize:10, //basescript
+        previous: ["Script1.2.5"],
+        link:"www.thisisalink2.com",
+        dateReleased: new Date("18-02-16"), 
+        downloads: 8, 
+        lastDownload: new Date("20-05-22"),
+        export: true,
+        comments: [],
+        source: {name:"fileName3", key:"key313", location:"filelocation3"}
+      })
+    });
+  });
+
+  describe('alreadyExists', ()=>{
+    it('should check if there is already a collection', ()=>{
+      const newUser: user ={
+        name: "Jennifer",
+        email: "jenniferaustin@gmail.com"
+      };
+      expect(controller.alreadyExists(newUser.name, newUser.email,"Collection20")).resolves.toEqual(true)
+    });
+  });
 })
