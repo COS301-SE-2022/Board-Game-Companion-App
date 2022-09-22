b6cf0548237e5ea47a97f69e1a324e3f3f351f36
Joseph-Makgopa <u18166793@tuks.co.za>
2022-09-23 00:52:15 +0200

    (admin):created counting functions

--- libs/api-lib/src/lib/controllers/admin/admin.controller.ts
+++ libs/api-lib/src/lib/controllers/admin/admin.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Body,  Get, Query, Post, Put, Delete, Req ,UploadedFile, UseInterceptors, Param, HttpException, HttpStatus } from '@nestjs/common';
-import { AlertService } from '../../services/alert/alert.service';
+import { AdminService } from '../../services/admin/admin.service';
 import { AlertDocument } from '../../schemas/alert.schema';
 import { user } from '../../models/general/user';
 
 
 @Controller('admin')
-export class ApiAlertController {
-    constructor(private readonly alertService:AlertService){}    
+export class ApiAdminController {
+    constructor(private readonly adminService:AdminService){}    
     
 }
--- libs/api-lib/src/lib/services/admin/admin.service.ts
+++ libs/api-lib/src/lib/services/admin/admin.service.ts
@@ -1,59 +1,33 @@
 import { Injectable } from '@nestjs/common';
-import { Model } from 'mongoose';
+import { Collection, Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { Alert, AlertDocument } from '../../schemas/alert.schema';
 import { user } from '../../models/general/user';
 import { AutomataScript, AutomataScriptDocument } from '../../schemas/automata-script.schema';
 import { OldScript, OldScriptDocument } from '../../schemas/old-script.schema';
-import { alertType } from '../../models/general/alertType';
-import { alertDto } from '../../models/dto/alertDto';
-import {WebSocketGateway, WebSocketServer, SubscribeMessage, OnGatewayConnection, OnGatewayDisconnect} from '@nestjs/websockets';
-import { Server } from 'http';
+import { CollectionDocument } from '../../schemas/collection.schema';
+import { DownloadScript, DownloadScriptDocument } from '../../schemas/download-script.schema';
+import { MyScript, MyScriptDocument } from '../../schemas/my-script.schema';
 
-@WebSocketGateway({cors: true})
 @Injectable()
-export class AlertService {
-    @WebSocketServer()server: Server;
+export class AdminService {
 
-    constructor(@InjectModel(Alert.name) private readonly alertModel: Model<AlertDocument>,
-                @InjectModel(AutomataScript.name) private readonly automataModel: Model<AutomataScriptDocument>,
-                @InjectModel(OldScript.name) private readonly oldModel: Model<OldScriptDocument>){}
+    constructor(@InjectModel(AutomataScript.name) private readonly automataModel: Model<AutomataScriptDocument>,
+                @InjectModel(MyScript.name) private readonly myscriptModel: Model<MyScriptDocument>,
+                @InjectModel(DownloadScript.name) private readonly downloadModel: Model<DownloadScriptDocument>,
+                @InjectModel(OldScript.name) private readonly oldModel: Model<OldScriptDocument>,
+                @InjectModel(Collection.name)private readonly collectionModel: Model<CollectionDocument>,){}
 
-    
 
-    async create(recepient:user,link:string,type:alertType):Promise<AlertDocument>{
-        const dto:alertDto = {
-            recepient:recepient,
-            date: new Date(),
-            link:link,
-            alertType:type,
-            read:false
-        }
-
-        const created = new this.alertModel(dto);
-        
-        const result = await created.save();
-        this.server.emit("alert." + result.recepient.name + "." + result.recepient.email,result);
-        return result;
+    async countDownloaders():Promise<number>{
+        return (await this.downloadModel.distinct("owner.email")).length;
     }
 
-    async getAllUserMessages(recepient:user):Promise<AlertDocument[]>{
-        return this.alertModel.find({"recepient.name":recepient.name,"recepient.email":recepient.email});
+    async countCollectionOwners():Promise<number>{
+        return (await this.collectionModel.distinct("owner.email")).length
     }
 
-    async getAllUnReadUserMessages(recepient:user):Promise<AlertDocument[]>{
-        return this.alertModel.find({"recepient.name":recepient.name,"recepient.email":recepient.email,"read":false});
-    }
-
-    async markAsRead(id:string):Promise<AlertDocument>{
-        const result = await this.alertModel.findById(id);
-        
-        if(result === null || result === undefined)
-            return null;
-
-        result.read = true;
-
-        await result.save();
-        return result;
+    async countScriptAuthors():Promise<number>{
+        return (await this.myscriptModel.distinct("author.email")).length
     }
 }
