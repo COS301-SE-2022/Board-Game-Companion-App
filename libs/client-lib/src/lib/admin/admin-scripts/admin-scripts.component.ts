344d932dad206c3c0d77a1b9b6d2dfcf8b8eb87b
Joseph-Makgopa <u18166793@tuks.co.za>
2022-09-23 00:00:10 +0200

    (admin):added pie chart in users section

--- libs/client-lib/src/lib/admin/admin-scripts/admin-scripts.component.ts
+++ libs/client-lib/src/lib/admin/admin-scripts/admin-scripts.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChildren,ViewChild,QueryList } from '@angular/core';
 import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 import { NotificationComponent } from '../../shared/components/notification/notification.component';
 import { automataScript } from '../../shared/models/scripts/automata-script';
 import { downloadScript } from '../../shared/models/scripts/download-script';
 import { myScript } from '../../shared/models/scripts/my-script';
+import { oldScript } from '../../shared/models/scripts/old-script';
 import { ScriptService } from '../../shared/services/scripts/script.service';
 
 @Component({
@@ -14,7 +15,7 @@ import { ScriptService } from '../../shared/services/scripts/script.service';
 })
 export class AdminScriptsComponent implements OnInit{
   selectedYear = (new Date()).getFullYear();
-  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+  @ViewChildren(BaseChartDirective) chart!: QueryList<BaseChartDirective>;
   @ViewChild(NotificationComponent,{static:true}) notifications: NotificationComponent = new NotificationComponent();
   barChartOptions: ChartConfiguration['options'] = {
     responsive: true,
@@ -31,7 +32,6 @@ export class AdminScriptsComponent implements OnInit{
     }
   };
   barChartType: ChartType = 'bar';
-
   barChartData: ChartData<'bar'> = {
     labels: [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ],
     datasets: [
@@ -40,7 +40,22 @@ export class AdminScriptsComponent implements OnInit{
       { data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], label: 'Released'}
     ]
   };
-
+  pieChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true,
+        position: 'left',
+      }
+    }
+  };
+  pieChartData: ChartData<'pie', number[], string | string[]> = {
+    labels: [ 'downloads', 'In Progress', 'Flagged', 'Released','Old Versions' ],
+    datasets: [ {
+      data: [ 0, 0, 0, 0, 0 ]
+    } ]
+  };
+  pieChartType: ChartType = 'pie';
   
   constructor(private readonly scriptService:ScriptService){}
 
@@ -58,8 +73,11 @@ export class AdminScriptsComponent implements OnInit{
             this.barChartData.datasets[1].data[vDate.getMonth() - 1] += 1;
         });
 
-        
-        this.chart?.update();
+        this.pieChartData.datasets[0].data[0] = value.length;
+
+        this.chart.forEach((element) => {
+          element.update();
+        })
       },
       error:() => {
         this.notifications.add({type:"warning",message:"Failed to retreive all the downloaded script"})
@@ -68,15 +86,25 @@ export class AdminScriptsComponent implements OnInit{
 
     this.scriptService.getAllMyScript().subscribe({
       next:(value:myScript[]) => {
+        let inProgress = 0;
+        let Flagged = 0;
+
         value.forEach((script:myScript) => {
           const vDate = new Date(script.created);
 
           if(this.selectedYear === vDate.getFullYear())
             this.barChartData.datasets[1].data[vDate.getMonth() - 1] += script.status.value === 1 ? 1 : 0;
+        
+          inProgress += script.status.value === 1 ? 1 : 0;
+          Flagged += script.status.value === 0 ? 1 : 0;
         });
 
-        
-        this.chart?.update();
+        this.pieChartData.datasets[0].data[1] = inProgress;
+        this.pieChartData.datasets[0].data[2] = Flagged;
+
+        this.chart.forEach((element) => {
+          element.update();
+        })
       },
       error:() => {
         this.notifications.add({type:"warning",message:"Failed to retreive all the in progress script"})
@@ -92,11 +120,26 @@ export class AdminScriptsComponent implements OnInit{
             this.barChartData.datasets[2].data[vDate.getMonth()] += 1;
         });
 
-        
-        this.chart?.update();
+        this.pieChartData.datasets[0].data[3] = value.length;
+
+        this.chart.forEach((element) => {
+          element.update();
+        })
       },
       error:() => {
-        this.notifications.add({type:"warning",message:"Failed to retreive all the released script"})
+        this.notifications.add({type:"warning",message:"Failed to count all the released scripts."})
+      }
+    })
+
+    this.scriptService.getOldScripts().subscribe({
+      next:(value:oldScript[]) =>{
+        this.pieChartData.datasets[0].data[4] = value.length;
+        this.chart.forEach((element) => {
+          element.update();
+        })
+      },
+      error:()=>{
+        this.notifications.add({type:"warning",message:"Failed to count all the old scripts"})
       }
     })
   }
--- libs/client-lib/src/lib/admin/admin-users/admin-users.component.ts
+++ libs/client-lib/src/lib/admin/admin-users/admin-users.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'board-game-companion-app-admin-users',
@@ -6,7 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-users.component.scss'],
 })
 export class AdminUsersComponent implements OnInit {
-  
+  @ViewChild(BaseChartDirective) chart!: BaseChartDirective;
+  pieChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true,
+        position: 'left',
+      }
+    }
+  };
+  pieChartData: ChartData<'pie', number[], string | string[]> = {
+    labels: [ 'Collection Owners', 'Script Authors', 'Script Downloaders'],
+    datasets: [ {
+      data: [ 0, 0, 0 ]
+    } ]
+  };
+  pieChartType: ChartType = 'pie';
   ngOnInit(): void {
     console.log("admin")
   }
