3761c46730640ce6a99dfb11957fea212f3716ef
QabaNjabulo <u19062665@tuks.co.za>
2022-09-23 10:51:27 +0200

    alert service integrates with the api.

--- libs/client-lib/src/lib/shared/services/alert/alert.service.spec.ts
+++ libs/client-lib/src/lib/shared/services/alert/alert.service.spec.ts
@@ -1,23 +1,47 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientModule } from '@angular/common/http';
 import { AlertService } from './alert.service';
 import { SocketIoModule } from 'ngx-socket-io';
-// import { WrappedSocket } from 'ngx-socket-io/src/socket-io.service';
-// import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
+import { take } from 'rxjs/operators';
+// import { alert } from '../../models/alert/alert';
 
-
+/************* INTEGRATION TESTS ****************/
 describe('Test service',()=>{
 
   let service: AlertService;
-  // const IO_CONFIG:SocketIoConfig = {url:'http://localhost:3000,options:{}}
   beforeEach(()=>{
-    TestBed.configureTestingModule({imports:[HttpClientTestingModule,SocketIoModule.forRoot({ url: 'http://localhost:3333/api', options: { transports: ['websocket'], reconnection: true } })],
+    TestBed.configureTestingModule({imports:[HttpClientModule,SocketIoModule.forRoot({ url: 'http://localhost:3333/api', options: { transports: ['websocket'], reconnection: true } })],
     providers:[AlertService]});
     service = TestBed.inject(AlertService);
+    window.sessionStorage.clear();
+    window.localStorage.clear();
   });
 
   it('should create a service',()=>{
     expect(service).toBeTruthy();
+  });
+
+  it('should return an array of size 0 or greater',(done)=>{
+    window.sessionStorage.setItem('name','Matome Makgopa');
+    window.sessionStorage.setItem('email','matomemakgopa64@gmail.com');
+    service.getAllUserMessages().pipe(take(1)).subscribe(data=>{
+      expect(data.length).toBeGreaterThanOrEqual(0);
+      if(data.length!==0)
+        service.markAsRead(data[0]._id).subscribe(val=>{
+          expect(val).toBeTruthy();
+        });
+      done();
+    }
+    );
+  });
+
+  it('should post an alert and return it',(done)=>{
+    window.sessionStorage.setItem('name','Matome Makgopa');
+    window.sessionStorage.setItem('email','matomemakgopa64@gmail.com');
+    service.getAllUnReadUserMessages().subscribe(Alert=>{
+      expect(Alert).toBeDefined();
+      done();
+    });
   });
 
 });
