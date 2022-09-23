3770307b3f5cb4ea556dfabc150712cd7fa80c19
Joseph-Makgopa <u18166793@tuks.co.za>
2022-09-23 14:12:55 +0200

    (admin):added admin service to the client

--- libs/client-lib/src/lib/admin/admin-users/admin-users.component.ts
+++ libs/client-lib/src/lib/admin/admin-users/admin-users.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
+import { AdminService } from '../../shared/services/admin/admin.service';
+import { NotificationComponent } from '../../shared/components/notification/notification.component';
 
 @Component({
   selector: 'board-game-companion-app-admin-users',
@@ -9,6 +11,7 @@ import { BaseChartDirective } from 'ng2-charts';
 })
 export class AdminUsersComponent implements OnInit {
   @ViewChild(BaseChartDirective) chart!: BaseChartDirective;
+  @ViewChild(NotificationComponent,{static:true}) notifications: NotificationComponent = new NotificationComponent();
   pieChartOptions: ChartConfiguration['options'] = {
     responsive: true,
     plugins: {
@@ -25,8 +28,42 @@ export class AdminUsersComponent implements OnInit {
     } ]
   };
   pieChartType: ChartType = 'pie';
+
+  constructor(private readonly adminService:AdminService){}
+
   ngOnInit(): void {
-    console.log("admin")
+    this.initData();
   }
 
+  initData(): void{
+    this.adminService.countCollectionOwners().subscribe({
+      next:(value:number) => {
+        this.pieChartData.datasets[0].data[0] = value;
+        this.chart?.update();
+      },
+      error:()=>{
+        this.notifications.add({type:"warning",message:"Failed to count collections"});
+      }
+    })
+
+    this.adminService.countScriptAuthors().subscribe({
+      next:(value:number) => {
+        this.pieChartData.datasets[0].data[1] = value;
+        this.chart?.update();
+      },
+      error:()=>{
+        this.notifications.add({type:"warning",message:"Failed to count script authors"});
+      }
+    })
+
+    this.adminService.countDownloaders().subscribe({
+      next:(value:number) => {
+        this.pieChartData.datasets[0].data[2] = value;
+        this.chart?.update();
+      },
+      error:()=>{
+        this.notifications.add({type:"warning",message:"Failed to count downloaders"});
+      }
+    })
+  }
 }
---
+++ libs/client-lib/src/lib/shared/services/admin/admin.service.ts
@@ -0,0 +1,57 @@
+import { Injectable, NgModule } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Socket } from 'ngx-socket-io';
+import { moderator } from '../../models/admin/moderator';
+import { user } from '../../models/general/user';
+
+@Injectable()
+export class AdminService {
+  private api = "";
+
+  constructor(private readonly httpClient:HttpClient,
+              private readonly socket: Socket) { 
+    this.api = "http://localhost:3333/api/";
+    //this.api = "https://board-game-companion-app-api.herokuapp.com/api/"
+  }
+
+  create(email:string):Observable<moderator>{
+      return this.httpClient.post<moderator>(this.api + "admin/create",{email:email});
+  }
+
+  setAdmin(id:string,admin:boolean):Observable<moderator>{
+      return this.httpClient.put<moderator>(this.api + "admin/set-admin",{id:id,admin:admin}); 
+  }
+
+  
+  getAll():Observable<moderator[]>{  
+      return this.httpClient.get<moderator[]>(this.api + "admin/retreive-all")
+  }
+
+  remove(id:string):Observable<moderator>{
+      let param = new HttpParams();
+      param = param.set("id",id);
+
+      return this.httpClient.delete<moderator>(this.api + "admin/remove",{params: param});
+  }
+
+  search(term:string):Observable<user[]>{
+      let param = new HttpParams();
+      param = param.set("term",term);
+
+      return this.httpClient.get<user[]>(this.api + "admin/search",{params: param});
+  }
+
+  countDownloaders():Observable<number>{
+      return this.httpClient.get<number>(this.api + "admin/count-downloaders");
+  }
+
+  countCollectionOwners():Observable<number>{
+      return this.httpClient.get<number>(this.api + "admin/count-collection-owners");
+  }
+
+  
+  countScriptAuthors():Observable<number>{
+      return this.httpClient.get<number>(this.api + "admin/count-script-authors");
+  }
+}
