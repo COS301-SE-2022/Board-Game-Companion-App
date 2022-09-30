c97c02b552389e99f754fa7fb28f28839750b1fd
QabaNjabulo <u19062665@tuks.co.za>
2022-09-30 06:16:16 +0200

    dependency fix

--- libs/client-lib/src/lib/admin/admin/admin.component.spec.ts
+++ libs/client-lib/src/lib/admin/admin/admin.component.spec.ts
@@ -4,6 +4,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AdminComponent } from './admin.component';
 import { FormsModule } from '@angular/forms';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('AdminComponent', () => {
   let component: AdminComponent;
@@ -13,7 +14,8 @@ describe('AdminComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [AdminComponent],
       providers: [],
-      imports: [HttpClientTestingModule,RouterTestingModule,NgxPaginationModule,FormsModule]
+      imports: [HttpClientTestingModule,RouterTestingModule,NgxPaginationModule,FormsModule],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   });
 
